5fd65ed3eea026174ccb40eb61bfa5e29d120829
Enrique Canel <enriquecanel1998@gmail.com>
2024-05-14 04:48:49 +0000

    Completed typescript primitives and variables

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,18 @@
+const fullName: string = 'Enrique';
+const isCool: boolean = true;
+const totalPets: number = 3;
+console.log('My name is ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets ', totalPets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets ', typeof totalPets);
+
+const empty: null = null;
+console.log('value of empty ', empty);
+console.log('typeof empty: ', typeof empty);
+
+let nothing: undefined;
+console.log('value of nothing: ', nothing);
+console.log('typeof of nothing ', typeof nothing);
