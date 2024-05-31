ad0584fe504c7f3e27c7585464e209869525f995
Enrique Canel <enriquecanel1998@gmail.com>
2024-05-31 03:15:19 +0000

    in progress

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,2 +1,11 @@
 "use strict";
 /* exported compact */
+function compact(array) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === true && array[i] !== null && array[i] !== isNaN && array[i] !== 0 && array[i] !== undefined && array[i] !== "") {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
--- array-manipulation-1/src/compact.ts
+++ array-manipulation-1/src/compact.ts
@@ -1,1 +1,10 @@
 /* exported compact */
+function compact(array: unknown[]):unknown[]{
+  const newArray: unknown[] = [];
+  for(let i:number = 0; i < array.length; i++){
+    if(array[i] === true && array[i] !== null && array[i] !== isNaN && array[i] !== 0 && array[i] !== undefined && array[i] !== ""){
+    newArray.push(array[i]);
+    }
+  }
+return newArray;
+}
