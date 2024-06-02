d1ddf2532fc675b6465919cf7ff0327ccf686ac7
Enrique Canel <enriquecanel1998@gmail.com>
2024-06-02 03:19:03 +0000

    Completed array manipulation 1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,11 +1,18 @@
-"use strict";
+'use strict';
 /* exported compact */
 function compact(array) {
-    const newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === true && array[i] !== null && array[i] !== isNaN && array[i] !== 0 && array[i] !== undefined && array[i] !== "") {
-            newArray.push(array[i]);
-        }
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (
+      array[i] !== false &&
+      array[i] !== null &&
+      !Number.isNaN(array[i]) &&
+      array[i] !== 0 &&
+      array[i] !== undefined &&
+      array[i] !== ''
+    ) {
+      newArray.push(array[i]);
     }
-    return newArray;
+  }
+  return newArray;
 }
--- array-manipulation-1/src/compact.ts
+++ array-manipulation-1/src/compact.ts
@@ -1,10 +1,17 @@
 /* exported compact */
-function compact(array: unknown[]):unknown[]{
+function compact(array: unknown[]): unknown[] {
   const newArray: unknown[] = [];
-  for(let i:number = 0; i < array.length; i++){
-    if(array[i] === true && array[i] !== null && array[i] !== isNaN && array[i] !== 0 && array[i] !== undefined && array[i] !== ""){
-    newArray.push(array[i]);
+  for (let i: number = 0; i < array.length; i++) {
+    if (
+      array[i] !== false &&
+      array[i] !== null &&
+      !Number.isNaN(array[i]) &&
+      array[i] !== 0 &&
+      array[i] !== undefined &&
+      array[i] !== ''
+    ) {
+      newArray.push(array[i]);
     }
   }
-return newArray;
+  return newArray;
 }
