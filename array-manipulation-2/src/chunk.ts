516dba49cc47ccb41b62910497436898461c0399
Enrique Canel <enriquecanel1998@gmail.com>
2024-06-06 03:32:37 +0000

    Completed array manipulation 2

--- array-manipulation-2/src/chunk.ts
+++ array-manipulation-2/src/chunk.ts
@@ -1,10 +1,15 @@
 /* exported chunk */
-function chunk(array:unknown[], size:number): unknown[]{
-  const newArrays:unknown[] = [];
-  for(let i:number = 0; i < size; i++){
-    if(i < array.length - size){
-    const newArray: unknown[] = array.slice(i);
-      newArrays.push(newArray);
+function chunk(array: unknown[], size: number): unknown[] {
+  const newArrays: unknown[] = [];
+  let leftovers: unknown[] = [];
+  for (let i: number = 0; i < array.length; i++) {
+    if (i % size === 0) {
+      newArrays.push([array[i]]);
+      // to chunk the arrays
+    } else {
+      leftovers = newArrays[newArrays.length - 1];
+      // make the chunk
+      leftovers.push(array[i]);
     }
   }
   return newArrays;
