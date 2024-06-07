94b574e6cc056e6ee51f3be3fcfc00e41f808986
Enrique Canel <enriquecanel1998@gmail.com>
2024-06-07 00:46:32 +0000

    in progress

---
+++ string-manipulation-2/src/truncate.js
@@ -0,0 +1,9 @@
+"use strict";
+/* exported truncate */
+function truncate(length, string) {
+    const newString = "";
+    for (let index = 0; index < string.length; index++) {
+        string[index];
+    }
+    return newString;
+}
---
+++ string-manipulation-2/src/truncate.ts
@@ -0,0 +1,9 @@
+/* exported truncate */
+function truncate(length:number, string:string):string{
+ const newString:string = "";
+ for(let index:number = 0; index < string.length; index ++){
+  string[index];
+
+ }
+ return newString;
+}
