c3106630d7645e147df465e04d94f0bd886cd13d
Enrique Canel <enriquecanel1998@gmail.com>
2024-06-11 00:20:52 +0000

    in progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,2 +1,6 @@
 "use strict";
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+    const firstChar = word.charAt(0);
+    return firstChar.toUpperCase() + word.slice(1).toLowerCase();
+}
--- string-manipulation-2/src/capitalize-word.ts
+++ string-manipulation-2/src/capitalize-word.ts
@@ -1,1 +1,5 @@
 /* exported capitalizeWord */
+function capitalizeWord(word:string):string{
+  const firstChar:string = word.charAt(0);
+  return firstChar.toUpperCase() + word.slice(1).toLowerCase();
+}
