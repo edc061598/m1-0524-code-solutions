23efb95f4421965a45f567c73066dcea76b91a8d
Enrique Canel <enriquecanel1998@gmail.com>
2024-07-02 03:11:09 +0000

    Completed typescript modal

---
+++ typescript-modal/main.ts
@@ -0,0 +1,23 @@
+const $openModal = document.querySelector('.open-modal');
+if (!$openModal) throw new Error('$openModal does not exist');
+
+const $dismissModal = document.querySelector('.dismiss-modal');
+if (!$dismissModal) throw new Error('$dismissModal does not exist');
+
+const $dialog = document.querySelector('dialog');
+if (!$dialog) throw new Error('$dialog does not exist');
+
+function openModalFunction(): void {
+  const $dialog = document.querySelector('dialog');
+  $dialog?.showModal();
+  console.log($dialog?.showModal());
+}
+
+function closeModalFunction(): void {
+  const $dialog = document.querySelector('dialog');
+  $dialog?.close();
+  console.log($dialog?.close());
+}
+
+$openModal.addEventListener('click', openModalFunction);
+$dismissModal.addEventListener('click', closeModalFunction);
