668c88024e6932dc74c99cb9ad39b8e563780822
Enrique Canel <enriquecanel1998@gmail.com>
2024-07-04 02:28:24 +0000

    not done

--- typescript-forms/form-events.js
+++ typescript-forms/form-events.js
@@ -1,23 +1,26 @@
-'use strict';
+"use strict";
 const $handleFocus = document.querySelector('input');
-if (!$handleFocus) throw new Error('$input does not exist');
+if (!$handleFocus)
+    throw new Error('$input does not exist');
 const $handleBlur = document.querySelector('input');
-if (!handleBlur) throw new Error('$input does not exist');
+if (!handleBlur)
+    throw new Error('$input does not exist');
 const $handleInput = document.querySelector('input');
-if (!handleInput) throw new Error('$input does not exist');
+if (!handleInput)
+    throw new Error('$input does not exist');
 function handleFocus(event) {
-  console.log('focus event was fired ');
-  const $eventTarget = event.target;
-  console.log('event.target.name ', $eventTarget.name);
+    console.log('focus event was fired ');
+    const $eventTarget = event.target;
+    console.log('event.target.name ', $eventTarget.name);
 }
 function handleBlur(event) {
-  console.log('blur event was fired');
-  const $eventTarget = event.target;
-  console.log('event.target.name ', $eventTarget.name);
+    console.log('blur event was fired');
+    const $eventTarget = event.target;
+    console.log('event.target.name ', $eventTarget.name);
 }
 function handleInput(event) {
-  const $eventTarget = event.target;
-  console.log('value of name: ', $eventTarget.value);
+    const $eventTarget = event.target;
+    console.log('value of name: ', $eventTarget.value);
 }
 $handleFocus.addEventListener('focus', handleFocus);
 $handleBlur?.addEventListener('blur', handleBlur);
--- typescript-forms/form-values.js
+++ typescript-forms/form-values.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 const $form = document.querySelector('#contact-form');
-if (!$form) throw new Error('formElements does not exist');
+if (!$form)
+    throw new Error('formElements does not exist');
 function submitFunction(event) {
-  event.preventDefault();
-  const $formElements = $form.elements;
-  const name = $formElements.name.value;
-  const email = $formElements.email.value;
-  const message = $formElements.message.value;
-  const objectForm = { name, email, message };
-  console.log(objectForm);
-  $form.reset();
+    event.preventDefault();
+    const $formElements = $form.elements;
+    const name = $formElements.name.value;
+    const email = $formElements.email.value;
+    const message = $formElements.message.value;
+    const objectForm = { name, email, message };
+    console.log(objectForm);
+    $form.reset();
 }
 $form.addEventListener('submit', submitFunction);
