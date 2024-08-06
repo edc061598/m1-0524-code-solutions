e3890a416b75f5b8fd04c087a12a44c4a57493f0
Enrique Canel <enriquecanel1998@gmail.com>
2024-08-06 02:40:57 +0000

    Completed practice-interview-challenge

---
+++ practice-interview-challenge/main.js
@@ -0,0 +1,20 @@
+"use strict";
+function getRangeReport(start, end) {
+    let total = 0;
+    let odds = [];
+    let evens = [];
+    let range = [];
+    let average = 0;
+    for (let i = start; i <= end; i++) {
+        if (i % 2 !== 0) {
+            odds.push(i);
+        }
+        else {
+            evens.push(i);
+        }
+        range.push(i);
+        average = (start + end) / 2;
+        total += range.push(i) / 2;
+    }
+    return [total, odds, evens, range, average];
+}
---
+++ practice-interview-challenge/main.ts
@@ -0,0 +1,27 @@
+function getRangeReport(start:number, end:number):any {
+  let total:number = 0;
+  let odds:number[] = [];
+  let evens:number[] = [];
+  let range:number[] = [];
+  let average:number = 0;
+
+for(let i:number = start; i <= end; i++){
+
+  if(i % 2 !== 0){
+    odds.push(i);
+
+
+  } else{
+    evens.push(i);
+
+  }
+   range.push(i);
+
+
+   average = (start + end) / 2;
+  total += range.push(i) / 2;
+
+}
+
+return [total, odds, evens, range, average];
+}
