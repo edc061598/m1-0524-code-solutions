6856739e92c604fce4bdf082e3f9ef6ba9b1e82c
Enrique Canel <enriquecanel1998@gmail.com>
2024-08-07 02:25:20 +0000

    Complete

--- typescript-timers/interval.js
+++ typescript-timers/interval.js
@@ -1,13 +1,15 @@
-'use strict';
+"use strict";
 const $countdown = document.querySelector('.countdown-display');
-if (!$countdown) throw new Error('$countdown does not exist');
+if (!$countdown)
+    throw new Error('$countdown does not exist');
 let count = 4;
 const intervalId = setInterval(() => {
-  if (count <= 0) {
-    clearInterval(intervalId);
-    $countdown.textContent = '`Earth is belloowwwwww Us!~';
-  } else {
-    $countdown.textContent = count.toString();
-    count--;
-  }
+    if (count <= 0) {
+        clearInterval(intervalId);
+        $countdown.textContent = '`Earth is belloowwwwww Us!~';
+    }
+    else {
+        $countdown.textContent = count.toString();
+        count--;
+    }
 }, 1000);
--- typescript-timers/timeout.js
+++ typescript-timers/timeout.js
@@ -1,6 +1,7 @@
-'use strict';
+"use strict";
 const $message = document.querySelector('.message');
-if (!$message) throw new Error('$message does not exist');
+if (!$message)
+    throw new Error('$message does not exist');
 setTimeout(function helloWorld() {
-  $message.textContent = 'HELLO THERE';
+    $message.textContent = 'HELLO THERE';
 }, 2000);
