98e8b5397301dd205217d5c5efcc71373a245f29
Enrique Canel <enriquecanel1998@gmail.com>
2024-08-13 01:27:20 +0000

    in progress

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -1,20 +1,44 @@
 "use strict";
-function initializeCarousel() {
-    const $carousel = document.querySelector('.carousel');
-    const $images = document.querySelectorAll('.carousel-images');
-    if (!$images)
-        throw new Error('$images does not exist');
-    const totalImages = $images.length;
-    let currentIndex = 0;
-    function showNextImage() {
-        if (!$carousel)
-            throw new Error('$carousel does not exist');
-        currentIndex = (currentIndex + 1) % totalImages;
-        const offset = -currentIndex * 100;
-        $carousel.style.transform = `translateX ${offset}%`;
+const $carouselList = document.querySelector('.carousel-list');
+if (!$carouselList)
+    throw new Error('$carouselList does not exist');
+const slides = Array.from($carouselList.children);
+const nextButton = document.querySelector('.fa-chevron-right');
+const prevButton = document.querySelector('.fa-chevron-left');
+const dotsNav = document.querySelector('.carousel-nav');
+if (!dotsNav)
+    throw new Error('dotsNav does not exist');
+const dots = Array.from(dotsNav.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+/*slides[0].style.left = 0;
+slides[1].style.left = slideWidth + 'px';
+slides[2].style.left = slideWidth * 2 + 'px';*/
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+prevButton?.addEventListener('click', () => {
+    if (!$carouselList)
+        throw new Error('$carouselList does not exist');
+    const currentSlide = $carouselList.querySelector('.current-slide');
+    const prevSlide = currentSlide?.previousElementSibling;
+    const amountToMove = prevSlide?.style.left;
+    $carouselList.style.transform = 'translateX(-' + amountToMove + ')';
+    currentSlide?.classList.remove('current-slide');
+    prevSlide?.classList.add('current-slide');
+});
+nextButton?.addEventListener('click', e => {
+    const currentSlide = $carouselList.querySelector('.current-slide');
+    const nextSlide = currentSlide?.nextElementSibling;
+    const amountToMove = nextSlide?.style.left;
+    $carouselList.style.transform = 'translateX(-' + amountToMove + ')';
+    currentSlide?.classList.remove('current-slide');
+    nextSlide?.classList.add('current-slide');
+});
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+    if (!targetDot) {
+        return;
     }
-    const interval = 3000;
-    setInterval(showNextImage, interval);
-    showNextImage();
-}
-document.addEventListener('DOMContentLoaded', initializeCarousel);
+    const currentSlide = $carouselList.querySelector('.current-slide');
+});
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -1,24 +1,54 @@
-function initializeCarousel():void{
+const $carouselList = document.querySelector('.carousel-list') as HTMLElement;
+if(!$carouselList) throw new Error('$carouselList does not exist');
 
-const $carousel = document.querySelector('.carousel');
+const slides = Array.from($carouselList.children);
 
-const $images = document.querySelectorAll('.carousel-images');
-if(!$images) throw new Error('$images does not exist');
+const nextButton = document.querySelector('.fa-chevron-right');
 
-const totalImages = $images.length;
-let currentIndex: number = 0;
+const prevButton=document.querySelector('.fa-chevron-left');
+const dotsNav = document.querySelector('.carousel-nav');
+if(!dotsNav) throw new Error('dotsNav does not exist');
 
-function showNextImage():void {
-  if(!$carousel) throw new Error('$carousel does not exist');
-  currentIndex = (currentIndex + 1) % totalImages;
-  const offset:number = -currentIndex * 100;
-  ($carousel as HTMLElement).style.transform = `translateX ${offset}%`;
+const dots = Array.from(dotsNav.children);
 
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+/*slides[0].style.left = 0;
+slides[1].style.left = slideWidth + 'px';
+slides[2].style.left = slideWidth * 2 + 'px';*/
+const setSlidePosition = (slide:any,index:number) => {
+  slide.style.left = slideWidth * index + 'px';
 }
 
-const interval:number = 3000;
-setInterval(showNextImage, interval);
-showNextImage();
-}
+slides.forEach(setSlidePosition);
 
-document.addEventListener('DOMContentLoaded', initializeCarousel);
+prevButton?.addEventListener('click', () => {
+  if(!$carouselList) throw new Error('$carouselList does not exist');
+  const currentSlide  = $carouselList.querySelector('.current-slide');
+  const prevSlide = currentSlide?.previousElementSibling as HTMLElement;
+  const amountToMove = prevSlide?.style.left;
+   $carouselList.style.transform = 'translateX(-' + amountToMove + ')';
+   currentSlide?.classList.remove('current-slide');
+   prevSlide?.classList.add('current-slide');
+
+})
+
+nextButton?.addEventListener('click', e => {
+  const currentSlide = $carouselList.querySelector('.current-slide');
+  const nextSlide = currentSlide?.nextElementSibling as HTMLElement;
+  const amountToMove = nextSlide?.style.left ;
+  $carouselList.style.transform = 'translateX(-' + amountToMove + ')';
+  currentSlide?.classList.remove('current-slide');
+  nextSlide?.classList.add('current-slide');
+})
+
+dotsNav.addEventListener('click', e => {
+  const targetDot = e.target.closest('button');
+  if(!targetDot){
+    return;
+  }
+  const currentSlide = $carouselList.querySelector('.current-slide');
+
+
+
+})
